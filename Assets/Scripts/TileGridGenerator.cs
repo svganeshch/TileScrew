d5cb8556d1deeb31a25d759ac86b7d080531d6b0
svganeshch <svganesh.chilakalapudi@gmail.com>
2025-01-04 17:48:43 +0530

    Implemented a tile grid generator with layers support

---
+++ Assets/Scripts/TileGridGenerator.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class TileGridGenerator : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public int rows = 5;
+    public int columns = 5;
+    public int layers = 1;
+    public float tileSpacing = 0.1f;
+
+    void Start()
+    {
+        for (int i = 0; i < layers; i++)
+        {
+            var layerRows = rows - i;
+            var layerColumns = columns - i;
+
+            GenerateGrid(layerRows, layerColumns, i);
+        }
+    }
+
+    void GenerateGrid(int rows, int columns, int layer)
+    {
+        float totalWidth = (columns - 1) * (1 + tileSpacing);
+        float totalHeight = (rows - 1) * (1 + tileSpacing);
+
+        float startPosX = -totalWidth / 2f;
+        float startPosY = totalHeight / 2f;
+
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < columns; x++)
+            {
+                Vector3 tilePosition = new Vector3(
+                    startPosX + x * (1 + tileSpacing),
+                    startPosY - y * (1 + tileSpacing),
+                    0
+                );
+
+                GameObject tile = Instantiate(tilePrefab, tilePosition, Quaternion.identity, transform);
+                tile.TryGetComponent<SpriteRenderer>(out SpriteRenderer tileSpriteRenderer);
+                tileSpriteRenderer.sortingOrder += layer;
+
+                tile.name = $"Tile_{x}_{y}";
+            }
+        }
+    }
+}
