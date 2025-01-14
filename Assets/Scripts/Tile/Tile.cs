9302e166434974ff94213a75e8fc68bd4306c5f1
svganeshch <svganesh.chilakalapudi@gmail.com>
2025-01-14 17:05:29 +0530

    Improve drop tile animation

--- Assets/Scripts/Tile/Tile.cs
+++ Assets/Scripts/Tile/Tile.cs
@@ -7,16 +7,20 @@ public class Tile : MonoBehaviour, ITile, ITouch
     public Color disabledColor;
     public Screw screw;
 
+    private Collider tileCollider;
     private SpriteRenderer spriteRenderer;
 
     private int m_tileLayer = 0;
     private bool state = true;
+
+    Sequence tileDropSequence;
 
     public int tileLayer { get => m_tileLayer; set => m_tileLayer = value; }
     public bool State { get => state; set => state = value; }
 
     private void Awake()
     {
+        tileCollider = GetComponent<Collider>();
         screw = GetComponentInChildren<Screw>();
 
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -52,10 +56,21 @@ public class Tile : MonoBehaviour, ITile, ITouch
 
     private void DropTile()
     {
-        transform.DOMove(Vector3.down * 10f, 0.5f)
-            .SetEase(Ease.OutQuad)
-            .OnComplete(
-                () => GameManager.Instance.tileGridGenerator.OnTileRemoved(this)
-             );
+        tileCollider.enabled = false;
+        tileDropSequence = DOTween.Sequence();
+
+        tileDropSequence.Insert(0, transform.DOScale(new Vector3(1.5f, 1.5f, 1.5f), 0.5f));
+
+        tileDropSequence.Insert(0, transform.DOLocalRotate(new Vector3(0, 0, 360), 0.5f, RotateMode.FastBeyond360)
+            .SetEase(Ease.Linear)
+            .SetLoops(3));
+
+        tileDropSequence.Insert(0.25f, transform.DOMove(transform.position + Vector3.up * 2f, 0.2f)
+            .SetEase(Ease.InQuad));
+
+        tileDropSequence.Insert(0.5f, transform.DOMove(transform.position + Vector3.down * 20f, 1f)
+            .SetEase(Ease.InOutQuad));
+
+        tileDropSequence.InsertCallback(0.1f, () => GameManager.Instance.tileGridGenerator.OnTileRemoved(this));
     }
 }
